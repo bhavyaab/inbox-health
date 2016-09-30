fde19e0d4c65cf4402ab1c2d8de276d1e3cb57fc
Bhavya Bharti <bhavyab@outlook.com>
2016-09-30 13:18:38 -0700

    package.json issue fixed

--- scripts/controller/userInput.js
+++ scripts/controller/userInput.js
@@ -1,4 +1,4 @@
-//validate input data
+//TODO validate input data need to be improved for error catching.
 function validateForm() {
   var x = document.forms['user_email']['email'].value;
   if (x == null || x == '') {
