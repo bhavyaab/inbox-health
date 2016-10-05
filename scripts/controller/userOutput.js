032fcb413ab7184bfe16456224824186573e7633
Rae Helsel <rahelsel@me.com>
2016-10-05 16:44:15 -0700

    fixing merge conflicts

--- scripts/controller/userOutput.js
+++ scripts/controller/userOutput.js
@@ -1,22 +1,77 @@
 /*6*/
-var allInfo = []; //eslint-disable-line
-var requireInfo = [];
+// var requireInfo = [];
 function GetInfo(id, from, unsubscribe,senderName){
   this.id = id;
   this.from = from;
   this.unsubscribe = unsubscribe;
   this.senderName = senderName;
 };
-var unique = [];
+
 var lookUpTable = {};
-var getUniqueSenders = function(){
-  requireInfo.forEach(function(item){
-    if(lookUpTable.hasOwnProperty(item.senderName)){}
-    else{
-      lookUpTable[item.senderName] = true;
-      unique.push(item);
-    };
-  });
+function noSubscribeHeader(currMessage) {
+  var raw;
+  if (currMessage.payload.parts) {
+    if (currMessage.payload.parts.length > 1) {
+      if (currMessage.payload.parts[1].body.data) {
+        try {
+          raw = currMessage.payload.parts[1].body.data.split(/[-_]/);
+        }
+        catch(e) {
+        }
+      }
+    } else {
+      if (currMessage.payload.parts[0].parts) {
+        try {
+          raw = currMessage.payload.parts[0].parts[1].body.data.split(/[-_]/);
+        }
+        catch(e) {
+        }
+      }
+    }
+  } else {
+    try {
+      raw = currMessage.payload.body.data.split(/[-_]/);
+    }
+    catch(e) {
+    }
+  }
+  if (raw) {
+    var newString = raw.reduce(function(acc, next) {
+      try {
+        return acc + (atob(next));
+      }
+      catch(e) {
+      }
+    }, '');
+    var unsubscribePosition = newString.search('unsubscribe');
+    if (unsubscribePosition === -1) {
+      unsubscribePosition = newString.search('opt out');
+    }
+    var cutTo;
+    if (unsubscribePosition - 1200 < 0) {
+      cutTo = 0;
+    } else {
+      cutTo = unsubscribePosition - 1200;
+    }
+    var linkString = newString.slice(cutTo, unsubscribePosition);
+    var allHrefs = linkString.split('href="');
+    var link;
+    if (allHrefs.length === 1) {
+      if (unsubscribePosition + 1200 > newString.length) {
+        cutTo = newString.length - 1;
+      } else {
+        cutTo = unsubscribePosition + 1200;
+      }
+      linkString = newString.slice(unsubscribePosition, cutTo);
+      allHrefs = linkString.split('href="');
+      if (allHrefs.length !== 1) {
+        link = allHrefs[1].split('"')[0];
+      }
+    } else {
+      link = allHrefs[allHrefs.length - 1].split('"')[0];
+    }
+  }
+  return link;
 };
 
 var generateInfo = function(resp){ //eslint-disable-line
@@ -38,14 +93,17 @@ var generateInfo = function(resp){ //eslint-disable-line
       unsubscribe = (unsubscribe.split('<')[1]).split('>')[0];
     };
   };
-  var possibleSenderName = from.split('@')[1].split('.');
-  senderName = (from.split('@')[1].split('.'))[possibleSenderName.length - 2];
-  if(senderName.length < 3){
-    senderName = (from.split('@')[1].split('.'))[possibleSenderName.length - 3];
+  senderName = from.split('<')[0];
+  if(from.includes('<')){
+    from = (from.split('<')[1]).split('>')[0];
   };
-  from = (from.split('<')[1]).split('>')[0];
   if(unsubscribe){
-    requireInfo.push(new GetInfo(id, from, unsubscribe, senderName));
+    if(lookUpTable.hasOwnProperty(senderName)){}
+    else{
+      lookUpTable[senderName] = true;
+      var template = Handlebars.compile($('#unsubscribe-template').html());
+      var uniqueObj = new GetInfo(id, from, unsubscribe, senderName);
+      $('#unsubscribe-page').append(template(uniqueObj));
+    };
   };
-  getUniqueSenders();
 };
