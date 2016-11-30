1471c75291314e81b52a0b168afa3f9c1dd1f060
Bhavya Bharti <bhavyab@outlook.com>
2016-11-30 11:02:53 -0800

    inbox-health information homapage compleate

--- scripts/controller/homeController.js
+++ scripts/controller/homeController.js
@@ -4,6 +4,7 @@
   homeController.index = function() {
     $('.tab-content').hide();
     $('#home-content').fadeIn();
+    $('#home-info').fadeIn();
     webDB.execute('SELECT * FROM emails INNER JOIN senderIds ON emails.sender = senderIds.sender', function(result) {
       if (result.length > 1) {
         $('#home-click').text('Unsubscribe').attr('href', '/unsubscribe');
