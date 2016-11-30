ddfefa5a90f99bcb6db1e80bea0520f1438c1f9e
Bhavya Bharti <bhavyab@outlook.com>
2016-11-30 15:30:08 -0800

    error for cheching profile is defined

--- scripts/view/appendTable.js
+++ scripts/view/appendTable.js
@@ -20,8 +20,10 @@
         var template = Handlebars.compile($('#unsubscribe-template').html());
         $('#unsubscribe-page ul').append(template(uniqueObj));
         $('#logout-button').fadeIn();
-        document.getElementById('profile-image').src = profile.imageUrl;
-        document.getElementById('profile-name').innerHTML = profile.displayName;
+        if(profile.imageUrl){
+          document.getElementById('profile-image').src = profile.imageUrl;
+          document.getElementById('profile-name').innerHTML = profile.displayName;
+        }
         anim.addValue('uniqueSenderNo');
         anim.insert('uniqueSenderNo');
       };
