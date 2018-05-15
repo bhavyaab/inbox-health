b8ac8e60e5c3d6c270d26b923554f59a58731de3
bhavyaab <bhavyab@outlook.com>
2018-05-15 11:58:24 -0700

    gmail push notification addition

---
+++ scripts/controller/watchGmail.js
@@ -0,0 +1,20 @@
+(function(module) {
+  var watchGamilController = {};
+
+  watchGamilController.notification = function() {
+    var accessToken = localStorage.getItem('accessToken');
+        $.ajax({
+          url: 'https://www.googleapis.com/gmail/v1/users/me/watch',
+          type: 'post',
+          headers: {'Content-Type': 'application/json'},
+          success: function(){
+            // console.log(' watch data recieved ')
+          }
+        }).fail(function(error){
+          console.log(error);
+          // something.wentWrong();
+        });
+}
+
+  module.watchGamilController = watchGamilController;
+})(window);
